88bf3a82a8b92646c4ee7b045d6d7292441c62e0
bukvoeshka <rezinalddobre@gmail.com>
2022-02-02 18:47:04 +0500

    calc finish

--- brain_games/calc.py
+++ brain_games/calc.py
@@ -1,43 +1,45 @@
 import random
 import prompt
 
-number = random.randint(1, 100)
-number1 = random.randint(1, 100)
-actions = ('+', '-', '*')
-action = random.choice(actions)
-question = f"{number} {action} {number1}"
-
 
 def welcome_user():
     global name
     name = prompt.string('May I have your name? ')
-    print('Hello, {}!'.format(name))
+    print(f'Hello, {name}!')
 
-
+# узнать насчёт способов облегчить функцию
 def quest():
-    """ global number
+    global number
+    global number1
+    global action
+    global answ
     number = random.randint(1, 100)
     number1 = random.randint(1, 100)
-    actions = ('+', '-', '*')
-    action = random.choice(actions)
-    global question
-    # question = '{} {} {}'.format(number, action, number1)
-    question = f"{number} {action} {number1}" """
+    action = random.choice(('+', '-', '*'))
+    question = f'{number} {action} {number1}'
     print('Question: ', question)
-    global answ
     answ = input('Your answer: ')
+
+
+def resl():
+    if action == '+':
+        return number + number1
+    elif action == '-':
+        return number - number1
+    elif action == '*':
+        return number * number1
 
 
 def check_answer():
     win_count = 0
     while win_count < 3:
         quest()
-        if answ == question:
+        if answ == str(resl()):
             print('Correct!')
             win_count += 1
         else:
-            print("'{}' is wrong answer ;(. Correct answer was '{}'.".format(answ, question))
-            print("Let's try again, {}!".format(name))
+            print(f"'{answ}' is wrong answer ;(. Correct answer was '{str(resl())}'.")
+            print(f"Let's try again, {name}!")
             break
     if win_count == 3:
-        print('Congrulations, {}!'.format(name))
+        print(f'Congrulations, {name}!')
